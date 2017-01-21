c4f053c516b061c6b99aa000f3a32e00a8a61906
Ludwig Gustavsson <ludwiggustavsson3@gmail.com>
2017-01-21 23:28:06 +0100

    Added main scene & basic scripts

---
+++ BulletPong/Assets/Scripts/FollowTransform.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour
+{
+    //Public vars
+    public Transform m_FollowTransform;
+    public float m_MaxDistance = 10.0f;
+
+    //Offset vars
+    private Vector3 m_Offset = Vector3.zero;
+
+    //Position vars
+    private Vector3 m_Destination = Vector3.zero;
+    private RaycastHit m_Hit;
+
+	void Start()
+    {
+		if (!m_FollowTransform)
+        {
+            Debug.Log("Followtransform is missing!");
+            enabled = false;
+            return;
+        }
+
+        m_Offset = m_FollowTransform.position - transform.position;
+	}
+	
+	void Update()
+    {
+        PositionUpdate();
+    }
+
+    void PositionUpdate()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out m_Hit, 100))
+        {
+            if (Vector3.Distance(m_FollowTransform.position, m_Hit.point) < m_MaxDistance)
+                m_Destination = (m_FollowTransform.position + m_Hit.point) / 2.0f - m_Offset;
+            else
+            {
+                Vector3 outline = m_FollowTransform.position + (m_Hit.point - m_FollowTransform.position).normalized * m_MaxDistance;
+                m_Destination = (m_FollowTransform.position + outline) / 2.0f - m_Offset;
+            }
+        }
+        else
+            m_Destination = m_FollowTransform.position - m_Offset;
+
+        transform.position = Vector3.Lerp(transform.position, m_Destination, 5 * Time.deltaTime);
+    }
+}
---
+++ BulletPong/Assets/Scripts/Player/ControllerPlayer.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(Collider))]
+public class ControllerPlayer : MonoBehaviour
+{
+    //Public vars
+    public float m_MovementSpeed = 20.0f;
+
+    //Component vars
+    private Rigidbody m_Rigidbody;
+    private Collider m_Collider;
+
+    //Movement vars
+    private float m_MaxSpeed = 0.0f;
+
+    //Rotation vars
+    private Transform m_Pointer;
+
+	void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Collider = GetComponent<Collider>();
+
+        m_MaxSpeed = m_MovementSpeed * m_MovementSpeed;
+
+        if (transform.FindChild("PointerRotation"))
+            m_Pointer = transform.FindChild("PointerRotation");
+        else
+            Debug.Log("Couldn't find pointer!");
+
+    }
+	
+	void Update()
+    {
+        MovementUpdate();
+        RotationUpdate();
+	}
+
+    void FixedUpdate()
+    {
+        MaxSpeedCheck();
+    }
+
+    void MaxSpeedCheck()
+    {
+        if (m_Rigidbody.velocity.sqrMagnitude > m_MaxSpeed)
+            m_Rigidbody.velocity = m_Rigidbody.velocity.normalized * m_MovementSpeed;
+    }
+
+    void MovementUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        m_Rigidbody.velocity = movement * m_MovementSpeed;
+    }
+
+    void RotationUpdate()
+    {
+        //Rotate towards mouseposition
+        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        m_Pointer.localRotation = Quaternion.AngleAxis(angle, Vector3.down);
+    }
+}
